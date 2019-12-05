a6c789bda6e309e35d6348134c60b7003b17f071
Denis <dmartynov@ecisys.com>
2019-12-06 02:12:30 +0300

    update path

--- face_rec/utils/face_identification.py
+++ face_rec/utils/face_identification.py
@@ -8,12 +8,12 @@ def isIdent(known, unknown):
         convertBase64ToFile(known, "knownImg.jpeg")
         convertBase64ToFile(unknown, "unknownImg.jpeg")
 
-        picture_of_me = face_recognition.load_image_file("/home/denis/Documents/templates/knownImg.jpeg") #"/home/denis/Documents/face-rec/DenisMartynov.jpg"
+        picture_of_me = face_recognition.load_image_file("/home/ec2-user/templates/knownImg.jpeg") #"/home/denis/Documents/face-rec/DenisMartynov.jpg"
         my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
         # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
 
-        unknown_picture = face_recognition.load_image_file("/home/denis/Documents/templates/unknownImg.jpeg") #"/home/denis/Documents/face-rec/Unknown.jpg"
+        unknown_picture = face_recognition.load_image_file("/home/ec2-user/templates/unknownImg.jpeg") #"/home/denis/Documents/face-rec/Unknown.jpg"
         face_recognition.load_image_file
         
         unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
@@ -28,7 +28,7 @@ def isIdent(known, unknown):
 def convertBase64ToFile(base64Str, imgName):
     image = base64.b64decode(str(base64Str))       
 
-    imagePath = ('/home/denis/Documents/templates/' + imgName)
+    imagePath = ('/home/ec2-user/templates/' + imgName)
     img = Image.open(io.BytesIO(image))
     img.save(imagePath, 'jpeg')
     
